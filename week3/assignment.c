ba00c1e7830b36ce177928198e5718984c9b916c
Prachotan Reddy Bathi <49813329+PrachotanReddy@users.noreply.github.com>
2020-01-27 11:17:25 +0530

    Add files via upload

---
+++ week3/assignment.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int a[20][20];
+	int i=0,j=0;
+	int m,n;
+	scanf("%d",&m);
+	scanf("%d",&n);
+	for(i=0;i<m;i++){
+		j=0;
+		for (j= 0; j<n; j++)
+		{
+			scanf("%d",&a[i][j]);
+		
+		}
+	}
+	int mincost;
+	int sum=0;
+
+
+}
